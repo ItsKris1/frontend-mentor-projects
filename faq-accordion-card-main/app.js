1bf0805d6d6ff12e0e39b7f079a48bd118940254
Kristofer Kangro <kkangro7@gmail.com>
2021-06-03 20:06:32 +0300

    questions arrow icons logic

--- faq-accordion-card-main/app.js
+++ faq-accordion-card-main/app.js
@@ -1,16 +1,31 @@
 const questions = document.querySelectorAll(".question");
+const arrows = document.querySelectorAll(".arrow-down");
 const answers = document.querySelectorAll(".answer");
 
 questions.forEach((question) => {
   question.addEventListener("click", () => {
-    // Remove show answer and from other questions
+    // If any other question winddo is open we want to close it;
+
     const currentAnswer = question.querySelector(".answer");
     answers.forEach((answer) => {
       if (answer !== currentAnswer) {
         answer.classList.remove("show-answer");
       }
     });
+    // If any other arrow is rotated, we want to rotate it back as it was;
+    const currentArrow = question.querySelector(".arrow-down");
+    arrows.forEach((arrow) => {
+      if (arrow !== currentArrow) {
+        arrow.style.transform = "rotate(360deg)";
+      }
+    });
 
-    currentAnswer.classList.toggle("show-answer");
+    const Toggle = currentAnswer.classList.toggle("show-answer");
+
+    if (Toggle) {
+      currentArrow.style.transform = "rotate(180deg)";
+    } else {
+      currentArrow.style.transform = "rotate(360deg)";
+    }
   });
 });
