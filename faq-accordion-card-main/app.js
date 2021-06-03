46dcda1a0f08f8a454ed12597e58a875fefb1e3c
Kristofer Kangro <kkangro7@gmail.com>
2021-06-03 18:05:32 +0300

    questions hover and focus states

--- faq-accordion-card-main/app.js
+++ faq-accordion-card-main/app.js
@@ -1,25 +1,16 @@
-const arrowDowns = document.querySelectorAll(".arrow-down");
+const questions = document.querySelectorAll(".question");
 const answers = document.querySelectorAll(".answer");
 
-arrowDowns.forEach((arrow) => {
-  arrow.addEventListener("click", (e) => {
-    const answer = e.currentTarget.parentElement.nextElementSibling;
-    answers.forEach((answ) => {
-      if (answ !== answer) {
-        answ.classList.remove("show-answer");
-        const currentArrow =
-          answ.previousElementSibling.querySelector(".arrow-down");
-        console.log(currentArrow);
-        currentArrow.style.transform = "rotate(360deg)";
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    // Remove show answer and from other questions
+    const currentAnswer = question.querySelector(".answer");
+    answers.forEach((answer) => {
+      if (answer !== currentAnswer) {
+        answer.classList.remove("show-answer");
       }
     });
-    console.log(!answer.classList.contains("show-answer"));
-    if (!answer.classList.contains("show-answer")) {
-      answer.classList.add("show-answer");
-      arrow.style.transform = "rotate(180deg)";
-    } else {
-      answer.classList.remove("show-answer");
-      arrow.style.transform = "rotate(360deg)";
-    }
+
+    currentAnswer.classList.toggle("show-answer");
   });
 });
